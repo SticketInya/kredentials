d1cdbc8c03ffe00f4fdd5fbb0a57c8857bf82ac4
JPatrik <jokhel.roland@gmail.com>
2025-03-18 20:02:58 +0100

    feat: add kredentials manager

---
+++ kredentials/errors.go
@@ -0,0 +1,11 @@
+package kredentials
+
+import "fmt"
+
+type ErrKredentialConflict struct {
+	Name string
+}
+
+func (e ErrKredentialConflict) Error() string {
+	return fmt.Sprintf("kredential '%s' already exists", e.Name)
+}
---
+++ kredentials/manager.go
@@ -0,0 +1,70 @@
+package kredentials
+
+import (
+	"fmt"
+
+	"github.com/SticketInya/kredentials/models"
+	"github.com/SticketInya/kredentials/storage"
+)
+
+const (
+	kubernetesConfigFilename string = "config"
+)
+
+type KredentialManager struct {
+	kredStore   storage.KredentialStore
+	configStore storage.KubernetesConfigStore
+}
+
+func NewKredentialManager(kredentialStore storage.KredentialStore, configStore storage.KubernetesConfigStore) *KredentialManager {
+	return &KredentialManager{
+		kredStore:   kredentialStore,
+		configStore: configStore,
+	}
+}
+
+func (m *KredentialManager) AddKredential(name string, path string) error {
+	kredentials, err := m.ListKredentials()
+	if err != nil {
+		return err
+	}
+
+	for _, existing := range kredentials {
+		if name == existing.Name {
+			return ErrKredentialConflict{name}
+		}
+	}
+
+	kubernetesConfig, err := m.configStore.LoadFromPath(path)
+	if err != nil {
+		return fmt.Errorf("loading kubernetes config: %w", err)
+	}
+
+	kred := models.NewKredential(name, kubernetesConfig)
+	return m.kredStore.Store(kred)
+}
+
+func (m *KredentialManager) LoadKredential(name string) (*models.Kredential, error) {
+	return m.kredStore.Load(name)
+}
+
+func (m *KredentialManager) ListKredentials() ([]*models.Kredential, error) {
+	return m.kredStore.List()
+}
+
+func (m *KredentialManager) DeleteKredential(name string) {
+	_ = m.kredStore.Delete(name)
+}
+
+func (m *KredentialManager) UseKredential(name string) error {
+	kred, err := m.kredStore.Load(name)
+	if err != nil {
+		return err
+	}
+
+	err = m.configStore.Store(kubernetesConfigFilename, *kred.Config)
+	if err != nil {
+		return fmt.Errorf("overwriting kubernetes config: %w", err)
+	}
+	return nil
+}
